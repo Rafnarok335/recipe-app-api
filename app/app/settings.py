67dfa70fe97b4975a61a2f0e3f7664e8c3d5e251
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2022-12-05 21:00:15 +0600

    section 1- v2

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,4 +1,4 @@
-# noqa: F401
+
 """
 Django settings for app project.
 
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os # Noqa
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
