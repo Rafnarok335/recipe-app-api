cca69280900396b6e2ee01b9d1fb20d9a95466d4
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2023-01-13 22:10:06 +0600

    section 13 v3

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,7 +12,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe Apis."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -27,3 +27,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
