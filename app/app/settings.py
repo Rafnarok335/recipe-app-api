076acd558ffb4cbc33518406fa03d057ed7e0998
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2023-02-02 13:41:39 +0600

    Docker compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dlvk9nrko4t@@9&p53vp6p&qa=)0)ec%*oo)p)r$vav$$x7b19'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
